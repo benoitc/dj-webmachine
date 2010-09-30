13a939ebc9c8f99cd9fae83aaee5f00285b9875b
Benoit Chesneau <bchesneau@gmail.com>
2010-09-30 19:55:19 +0200

    improve the example, add content_type detection and response.
    To test try:
    
    $ curl http://127.0.0.1:8000/hello
    <html><body>Hello world!</body></html>
    $ curl -H Accept:application/json http://127.0.0.1:8000/hello
    {"message": "hello world!", "ok": true}

--- example/testapi/foo/views.py
+++ example/testapi/foo/views.py
@@ -3,7 +3,19 @@
 
 from apipoint.resource import Resource
 
+import json
+
 class HelloRes(Resource):
+
+    def content_types_provided(self, req, resp):
+        return ( 
+            ("", self.to_html),
+            ("application/json", self.to_json)
+        )
 
     def to_html(self, req, resp):
         return "<html><body>Hello world!</body></html>"
+
+    def to_json(self, req, resp):
+        return json.dumps({"message": "hello world!", "ok": True})
+
