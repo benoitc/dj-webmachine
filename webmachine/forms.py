530c5989f3ea9d7b658162ec02f3a10bf6124e13
benoitc <bchesneau@gmail.com>
2010-10-29 20:36:59 +0200

    add form. based on piston form with slight modifications

---
+++ webmachine/forms.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -
+#
+# This file is part of dj-webmachine released under the MIT license. 
+# See the NOTICE for more information.
+
+import hmac
+try:
+    import hashlib
+    _sha = hashlib.sha1
+except ImportError
+    import sha
+    _sha = sha
+
+from django.conf import settings
+from django import forms
+
+
+class OAuthAuthenticationForm(forms.Form):
+    oauth_token = forms.CharField(widget=forms.HiddenInput)
+    oauth_callback = forms.CharField(widget=forms.HiddenInput, required=False)
+    authorize_access = forms.BooleanField(required=True)
+    csrf_signature = forms.CharField(widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        forms.Form.__init__(self, *args, **kwargs)
+
+        self.fields['csrf_signature'].initial = self.initial_csrf_signature
+
+    def clean_csrf_signature(self):
+        sig = self.cleaned_data['csrf_signature']
+        token = self.cleaned_data['oauth_token']
+
+        sig1 = OAuthAuthenticationForm.get_csrf_signature(settings.SECRET_KEY, 
+                token
+        if sig != sig1:
+            raise forms.ValidationError("CSRF signature is not valid")
+
+        return sig
+
+    def initial_csrf_signature(self):
+        token = self.initial['oauth_token']
+        return OAuthAuthenticationForm.get_csrf_signature(
+                settings.SECRET_KEY, token)
+
+    @staticmethod
+    def get_csrf_signature(key, token):
+        """ Check signature """
+        hashed = hmac.new(key, token, _sha)
+        return base64.b64encode(hashed.digest())
+
