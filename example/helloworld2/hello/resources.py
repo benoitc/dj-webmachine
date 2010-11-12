f98afe0f1d3fb8bdcac8750dd7b21bd13d90162c
benoitc <bchesneau@gmail.com>
2010-11-12 19:27:00 +0100

    all in one:
    @wm.route(r"^hello$", provided=["text/html", ("application/json",
    json.dumps)])
    def all_in_one(req, resp):
        if resp.content_type == "application/json":
            return {"ok": True, "message": "hellow world! All in one"}
        else:
            return "<html><p>hello world! All in one</p></html>"

--- example/helloworld2/hello/resources.py
+++ example/helloworld2/hello/resources.py
@@ -8,6 +8,13 @@ def hello(req, resp):
 
 @wm.route(r"^$", provided=[("application/json", json.dumps)])
 def hello_json(req, resp):
-    print "ici"
     return {"ok": True, "message": "hellow world"}
+
+@wm.route(r"^hello$", provided=["text/html", ("application/json", json.dumps)])
+def all_in_one(req, resp):
+    if resp.content_type == "application/json":
+        return {"ok": True, "message": "hellow world! All in one"}
+    else:
+        return "<html><p>hello world! All in one</p></html>"
+
 
